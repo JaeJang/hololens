77928dd24c935bd5771d004dd675705c78195c53
Jae Jang <jaejang5566@gmail.com>
2018-04-02 13:00:32 -0700

    updated

---
+++ HoloLens Moulage/Assets/MoveScene.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MoveScene : MonoBehaviour {
+
+    public AudioClip sound;
+    public GameObject ImageTarget;
+    public Transform menu;
+    private Transform currentImage;
+    private Button currentButton;
+
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void clickTest(Button btn)
+    {
+        //string name;
+        //name = EventSystem.current.currentSelectedGameObject.name;
+        if(currentImage != null)
+        {
+            currentImage.gameObject.SetActive(false);
+        }
+        string buttonName = btn.name;
+        currentImage = ImageTarget.transform.Find(buttonName);
+        currentImage.gameObject.SetActive(true);
+
+        if (currentButton != null)
+        {
+            ColorBlock cb1 = currentButton.colors;
+            cb1.normalColor = Color.white;
+            currentButton.colors = cb1;
+        }
+
+        currentButton = btn;
+        ColorBlock cb = currentButton.colors;
+        cb.normalColor = Color.blue;
+        currentButton.colors = cb;
+
+        menu.gameObject.SetActive(false);
+        
+
+    }
+
+    
+}
---
+++ HoloLens Moulage/Assets/SwitchMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchMenu : MonoBehaviour {
+
+    public GameObject panel;
+    private Button currentButton;
+    private Transform currentPanel;
+    
+	// Use this for initialization
+	void Start () {
+        currentPanel = panel.transform.Find("GunshotPanel");
+        currentPanel.gameObject.SetActive(true);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void clickButton(Button btn)
+    {
+
+        currentPanel.gameObject.SetActive(false);
+        if (btn.name.Equals("GunshotType"))
+        {
+            currentPanel = panel.transform.Find("GunshotPanel");
+
+        }
+        if (btn.name.Equals("SlashType"))
+        {
+            currentPanel = panel.transform.Find("SlashPanel");
+        }
+        if (btn.name.Equals("StabType"))
+        {
+            currentPanel = panel.transform.Find("StabPanel");
+        }
+        currentPanel.gameObject.SetActive(true);
+
+        if (currentButton != null)
+        {
+            ColorBlock cb1 = currentButton.colors;
+            cb1.normalColor = Color.black;
+            currentButton.colors = cb1;
+        }
+
+        currentButton = btn;
+        ColorBlock cb = currentButton.colors;
+        cb.normalColor = Color.blue;
+        currentButton.colors = cb;
+    }
+}
