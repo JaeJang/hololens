3abf5ac1f70ab2c28f769b031fc0e0ce1cfa3e59
Mark Tan <akirasendo7_13@yahoo.com>
2018-05-01 15:56:47 -0700

    ocmmitnign
    ocmmitnign changes to ui/ux

---
+++ HoloLens Moulage/Assets/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using HoloToolkit.Unity.InputModule;
+
+public class MainMenu : MonoBehaviour { 
+
+    public GameObject panel;
+    private Transform currentPanel;
+
+    public void openMenu()
+    {
+        this.gameObject.SetActive(false);
+        currentPanel = panel.transform.Find("Main");
+        currentPanel.gameObject.SetActive(true);
+    }
+
+    public void openWound(Button btn)
+    {
+        clearActivePanel();
+        if(btn.name.Equals("Gunshot"))
+        {
+            panel.transform.Find("GunshotOption").gameObject.SetActive(true);
+        } else if (btn.name.Equals("Stab"))
+        {
+            panel.transform.Find("StabOption").gameObject.SetActive(true);
+        } else if (btn.name.Equals("Slash"))
+        {
+            panel.transform.Find("SlashOption").gameObject.SetActive(true);
+        }
+    }
+
+    public void clearActivePanel()
+    {
+        panel.transform.Find("GunshotOption").gameObject.SetActive(false);
+        panel.transform.Find("StabOption").gameObject.SetActive(false);
+        panel.transform.Find("SlashOption").gameObject.SetActive(false);
+    }
+
+  
+}
---
+++ HoloLens Moulage/Assets/hover.cs
@@ -0,0 +1,32 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hover : MonoBehaviour, IFocusable {
+
+    public GameObject panel;
+    public void OnFocusEnter()
+    {
+        print("hello world");
+        panel.transform.Find("Yuck1").gameObject.SetActive(true);
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnFocusExit()
+    {
+        print("bye world");
+        panel.transform.Find("Yuck1").gameObject.SetActive(false);
+        //throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
